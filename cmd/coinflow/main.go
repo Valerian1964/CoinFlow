3a3232cc0b70724334e9522ab45953aa981f37a5
Valeri <35935215+Valerian1964@users.noreply.github.com>
2025-09-02 18:41:08 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/coinflow/main.go
@@ -0,0 +1,23 @@
+// cmd/coinflow/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "coinflow/internal/coinflow"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := coinflow.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
